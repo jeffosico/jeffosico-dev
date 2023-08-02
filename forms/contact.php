2531551e1ae6a7282d5eab533818a431c83b2936
Jeff Clyde Mark R. Osico <franch.osico@gmail.com>
2023-08-02 09:43:10 +0800

    fixed things

--- forms/contact.php
+++ forms/contact.php
@@ -25,13 +25,14 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
+  */
+
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'chrollo.lucilfer453@gmail.com',
+    'password' => 'izmatigaz',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
